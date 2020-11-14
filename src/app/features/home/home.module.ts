d32a23f4b1f4cb666417439ff63cdab4787fcdce
yamatoto <mftb35ymt@yahoo.co.jp>
2020-11-15 01:02:26 +0900

    generate features folder

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 import { HomePage } from './home.page';
 
-import { HomePageRoutingModule } from './home-routing.module';
-
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+  }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    RouterModule.forChild(routes)
   ],
   declarations: [HomePage]
 })
